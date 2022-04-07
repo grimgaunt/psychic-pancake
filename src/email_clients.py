ad603687b56c2eef2dea96fd1ba95b1d4113d049
Ryan-Knitter <ryan.knitter@thrivent.com>
2022-04-07 15:47:18 -0500

    Added SES Template Code

--- src/email_clients.py
+++ src/email_clients.py
@@ -1,5 +1,7 @@
 # Databricks notebook source
 import mlflow
+import boto3
+from botocore.exceptions import ClientError
 
 # COMMAND ----------
 
@@ -8,7 +10,7 @@ dbutils.widgets.text('run_dt','2022-04-06')
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC #### Pull Clients with Term-Life Contract Expiring in a Year
+# MAGIC #### Pull Clients Expiring Policy
 
 # COMMAND ----------
 
@@ -79,6 +81,7 @@ df = (
 
 # MAGIC %md
 # MAGIC #### Generate HTML Emails for All Clients and Save to S3
+# MAGIC Only generate for 1 client to illustrate the process.
 
 # COMMAND ----------
 
@@ -116,3 +119,72 @@ with open('/dbfs/FileStore/email_template.html', 'r') as f:
 # COMMAND ----------
 
 # displayHTML(html)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Send Email with AWS SES
+# MAGIC This integration is not currently setup with Databricks.
+
+# COMMAND ----------
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = "Sender Name <sender@example.com>"
+
+# Replace recipient@example.com with a "To" address. If your account 
+# is still in the sandbox, this address must be verified.
+RECIPIENT = recipient
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the 
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+CONFIGURATION_SET = "ConfigSet"
+
+# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "us-west-2"
+
+# The subject line for the email.
+SUBJECT = "Your Client has an Expiring Term-Life Policy"
+            
+# The HTML body of the email.
+BODY_HTML = html          
+
+# The character encoding for the email.
+CHARSET = "UTF-8"
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses',region_name=AWS_REGION)
+
+# Try to send the email.
+try:
+    #Provide the contents of the email.
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                RECIPIENT,
+            ],
+        },
+        Message={
+            'Body': {
+                'Html': {
+                    'Charset': CHARSET,
+                    'Data': BODY_HTML,
+                },
+            },
+            'Subject': {
+                'Charset': CHARSET,
+                'Data': SUBJECT,
+            },
+        },
+        Source=SENDER,
+        # If you are not using a configuration set, comment or delete the
+        # following line
+        ConfigurationSetName=CONFIGURATION_SET,
+    )
+# Display an error if something goes wrong.	
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
